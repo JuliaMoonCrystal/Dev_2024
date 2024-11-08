4c84056fe21d94a66cda8fdcbe8f11a0ee128f46
Julia Farias da Rocha <47614833+JuliaMoonCrystal@users.noreply.github.com>
2024-11-08 10:47:54 -0300

    update

---
+++ Python/Datas/datas.py
@@ -0,0 +1,10 @@
+from datetime import date, datetime,time
+
+# birthDate = datetime.date(2020,9,12) year,moth,day
+birthDate = date(2020,9,12)
+print(birthDate)
+print(birthDate.today())
+
+data_hora = dateTime(2023,7,10)
+
+
---
+++ Python/set/set.py
@@ -0,0 +1,21 @@
+set([1,2,3,5,1,2,6,6,8,10])
+
+# set tira elementos repetidos das listas 
+print(set([1,2,3,5,1,2,6,6,8,10]))
+
+numbers ={1,2,3,4,5,5}
+
+numeros =list(numbers)
+
+print(numeros[0])
+
+cars={"Gol","Palio","Celta"}
+
+for carro in cars:
+    print(carro)
+    
+for index, carro in enumerate(cars):
+    print(f"{index}: {carro}")    
+    
+    
+# metodos da classe set
